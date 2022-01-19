2e60906757f8e1a37c4e18c8bc379233f26af348
JoeSenna <joe.senna@live.com>
2022-01-19 11:33:21 -0300

    first commit

---
+++ resources/views/classes.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pokémon Red</title>
+    <style>
+        h1{
+            color: white;
+        }
+
+        body{
+            background-color: black;
+        }
+    </style>
+</head>
+<body>
+    <h3>Você escolheu: {{ $pkm }}</h3>
+    <img src="https://c.tenor.com/ZpBMkWyufhMAAAAC/dead.gif">
+</body>
+</html>
+<!--Criação dessa página #7-->
---
+++ resources/views/index.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pokémon Red</title>
+ 
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+        
+        h1{
+            color: white;
+        }
+        body {
+            height: 100vh;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            background-color: black;
+        }
+        
+        .container {
+            display: flex;
+        }
+ 
+        .container a div {
+            width: 200px;
+            height: 250px;
+            background-color: #ddd;
+            margin: 10px;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-evenly;
+            align-items: center;
+            border-radius: 10px;
+        }
+ 
+        .container a div img {
+            width: 180px;
+        }
+ 
+        .container a {
+            text-decoration: none;
+            color: #333;
+        }
+    </style>
+</head>
+<body>
+    <h1>Escolha um pokémon:</h1>
+    <div class="container">
+        <a href="/classes/charmander">
+            <div>
+                <img src="https://external-preview.redd.it/UddQUiXHYfyqTrLDdBjEKJJ56K7YdPOXWIn5Pm9RtLM.jpg?width=640&crop=smart&auto=webp&s=7ea2de68b631056c3dcee7de5dd8ae705917c770">
+                Charmander
+            </div>
+        </a>
+        <a href="/classes/squirtle">
+            <div>
+                <img src="https://i.pinimg.com/originals/ed/21/34/ed213482f982ca6e6e79e9584e15ca56.jpg">
+                Squirtle
+            </div>
+        </a>
+        <a href="/classes/bulbassaur">
+            <div>
+                <img src="https://external-preview.redd.it/ugbcabmJEdqDrkW-8jnTZG-_-GXiji2sYjdndqaMvx0.jpg?auto=webp&s=3aaf6e2f740491864b302e2add262a4c57e76949">
+                Bulbassaur
+            </div>
+        </a>
+    </div>
+</body>
+</html>
+<!--criação dessa pagina #06-->
